4b91c4d697b24806306530eb7b4a4a8388cb1242
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-25 15:35:28 +0530

    hello

---
+++ 04-variables-cl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "script name : $0"
+echo "first arguments : $1"
+echo "second argument : $2"
+echo "All arguments as separate word : $@"
+echo "All arguments as a single word : $*"
+echo "Number of arguments : $#"
