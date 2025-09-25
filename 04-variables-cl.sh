37c59062619a1bc812cf9a2a158f33920a6316e5
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-25 15:53:46 +0530

    hello

--- 04-variables-cl.sh
+++ 04-variables-cl.sh
@@ -6,3 +6,8 @@ echo "second argument : $2"
 echo "All arguments as separate word : $@"
 echo "All arguments as a single word : $*"
 echo "Number of arguments : $#"
+
+echo "Enter pin number"
+read PIN_NUMBER
+
+echo "The PIN number is : $PIN_NUMBER"
