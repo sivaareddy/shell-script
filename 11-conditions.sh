4c8a670807b65188a9c0ce3ddc78ee9d503631fc
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-28 08:04:43 +0530

    shell-practica

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,7 +3,7 @@
 echo "Please Enter a number"
 read NUMBER
 
-if [ $((NUMBER%2)) -eq 0 ]; then
+if [ $(($NUMBER%2)) -eq 0 ]; then
     echo "Given number is $NUMBER is EVEN"
 else 
     echo "Given number is $NUMBER is ODD"
