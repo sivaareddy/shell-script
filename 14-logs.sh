be89feaaf1d0ac6b886ad3fe14aab003bc2fc27f
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-28 22:05:54 +0530

    shell-practica

--- 14-logs.sh
+++ 14-logs.sh
@@ -7,10 +7,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOG_FOLDER="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
+mkdir -p $LOGS_FOLDER
 mkdir -p $LOGS_FOLDER
 
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
