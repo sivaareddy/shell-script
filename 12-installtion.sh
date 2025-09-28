de2fa7fc67309bdbef5eb2d5ba2a0418eaf409cf
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-28 21:09:12 +0530

    shell-practica

---
+++ 12-installtion.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -eq 0 ]; then
+    echo "Please run the script with root previliage"
+fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]; then
+    echo "Installtion mysql is success"
+else
+    echo "installation my sql is failure"
+fi
