fa97ac065749f2f8e515d679a4bf488b08f03e78
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-28 08:21:40 +0530

    shell-practica

--- prime.sh
+++ prime.sh
@@ -8,7 +8,7 @@ if [ $num -lt 2 ]; then
 fi
 
 for((i=2; i*i<=$num; i++)); do
-    if [ $num % i -eq 0 ]; then
+    if [ $(($num % i)) -eq 0 ]; then
         echo "$num is not prime"
         exit 1
     fi
