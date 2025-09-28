6deb227c301e6d3fb39a9df0e9afd583aeee3b4d
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-28 07:43:31 +0530

    shell-practice

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [$NUMBER -le 10]; then
+if [$NUMBER -le 10 ]; then
     echo "given number is $NUMBER less tha 10"
-elif [ @NUMBER -eq 10] ; then
+elif [ @NUMBER -eq 10 ] ; then
     echo "given number is $Number is equal to 10"
 else
     echo "Given number is $NUMBER greater than 10"
