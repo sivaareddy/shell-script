b0a84227f19b425e1fecf1c58e07f1992eeb4697
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-28 22:04:30 +0530

    shell-practica

--- 14-logs.sh
+++ 14-logs.sh
@@ -7,7 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOG_FOLDER="/var/logs/shell-script"
+LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
