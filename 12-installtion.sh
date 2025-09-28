9e5389760e628cfc7af2a8e0819279712eb539e5
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-28 21:45:47 +0530

    shell-practica

--- 12-installtion.sh
+++ 12-installtion.sh
@@ -3,26 +3,43 @@
 USERID=$(id -u)
 
 R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
-    echo -e "$R ERROR:: Please run this script with root privelege"
+    echo -e "$R ERROR:: Please run this script with root privelege $N"
     exit 1 # failure is other than 0
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "ERROR:: Installing $2 is failure"
+        echo "ERROR:: Installing $2 ........ $R FAILURE $N"
         exit 1
     else
-         echo "Installing $2 is SUCCESS"
+         echo "Installing $2 ......... $G SUCCESS $N"
     fi
 }
 
-dnf install mysql -y
-VALIDATE $? "MySQL"
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo -e "$Y MySQL already installed $N"
+fi
 
-dnf install nginx -y
-VALIDATE $? "Nginx"
-
-dnf install python3 -y
-VALIDATE $? "python3"
+dnf list installed nginx
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo "$Y MySQL already installed $N"
+fi
+dnf list installed python3
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "$Y MySQL already installed $N"
+fi
