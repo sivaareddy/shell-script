4a95d0200491010f872a64e030bc303da02a74f5
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-28 07:41:59 +0530

    shell-practice

---
+++ 10-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [$NUMBER -le 10]; then
+    echo "given number is $NUMBER less tha 10"
+elif [ @NUMBER -eq 10] ; then
+    echo "given number is $Number is equal to 10"
+else
+    echo "Given number is $NUMBER greater than 10"
+fi
