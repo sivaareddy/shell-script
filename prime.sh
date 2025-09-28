115d962fb747efaaaec1998634050cf0bb41fdc0
Sivanageswarareddy <sivanages.p@gmail.com>
2025-09-28 08:20:08 +0530

    shell-practica

---
+++ prime.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "enter number"
+read num
+
+if [ $num -lt 2 ]; then
+    echo "$num is not prime"
+    exit 1
+fi
+
+for((i=2; i*i<=$num; i++)); do
+    if [ $num % i -eq 0 ]; then\
+        echo "$num is not prime"
+        exit 1
+    fi
+done
+
+echo "$num is prime"
+exit 0
