1375c9ba6e3e168d1a1279fc0a8297f6d2b404b6
Sivanageswarareddy <sivanages.p@gmail.com>
2025-12-16 07:14:39 +0530

    shell

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -7,3 +7,6 @@ echo "Who running this ecript: $USER"
 echo "which folder we are in : $PWD"
 echo "current user homw direcory :$HOME"
 echo "PID of the script :$$"
+#& is used to below command to run in backgroun
+sleep 50 &
+echo "PID of the last command in backgound is :$!"
